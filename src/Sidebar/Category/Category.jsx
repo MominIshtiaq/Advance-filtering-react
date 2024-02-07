7a093d02abe1968b03b0240270fc9acc06d2fd20
MominIshtiaq <momin989ishtiaq@gmail.com>
2024-02-08 03:54:02 +0500

    Category section done

--- src/Sidebar/Category/Category.jsx
+++ src/Sidebar/Category/Category.jsx
@@ -1,7 +1,37 @@
 import "./Category.css";
 
 const Category = () => {
-  return <div>Category</div>;
+  return (
+    <>
+      <h2 className="sidebar_title">Category</h2>
+      <div>
+        <label className="sidebar_label_container">
+          <input type="radio" name="test" />
+          <span className="checkmark"></span>All
+        </label>
+
+        <label className="sidebar_label_container">
+          <input type="radio" name="test" />
+          <span className="checkmark"></span>Sneakers
+        </label>
+
+        <label className="sidebar_label_container">
+          <input type="radio" name="test" />
+          <span className="checkmark"></span>Flats
+        </label>
+
+        <label className="sidebar_label_container">
+          <input type="radio" name="test" />
+          <span className="checkmark"></span>Sandals
+        </label>
+
+        <label className="sidebar_label_container">
+          <input type="radio" name="test" />
+          <span className="checkmark"></span>Heels
+        </label>
+      </div>
+    </>
+  );
 };
 
 export default Category;
