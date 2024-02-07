57aa213ad6f368cf277b03572e440db276b6e57b
MominIshtiaq <momin989ishtiaq@gmail.com>
2024-02-08 00:19:34 +0500

    Navbar setup

--- src/Navigation/Nav.jsx
+++ src/Navigation/Nav.jsx
@@ -1,7 +1,27 @@
 import "./Nav.css";
+import { FiHeart } from "react-icons/fi";
+import { AiOutlineShoppingCart, AiOutlineUserAdd } from "react-icons/ai";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return;
+  <>
+    <nav>
+      <div className="nav_container">
+        <input type="text" placeholder="Search" className="search_input" />
+      </div>
+      <div className="profile_container">
+        <a href="#">
+          <FiHeart className="nav_icons" />
+        </a>
+        <a href="#">
+          <AiOutlineShoppingCart className="nav_icons" />
+        </a>
+        <a href="#">
+          <AiOutlineUserAdd className="nav_icons" />
+        </a>
+      </div>
+    </nav>
+  </>;
 };
 
 export default Nav;
