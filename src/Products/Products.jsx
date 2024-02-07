2cfff2e53ae480bc1672409fd604b467945d8d99
MominIshtiaq <momin989ishtiaq@gmail.com>
2024-02-08 03:14:03 +0500

    Products section structure and designing complete

--- src/Products/Products.jsx
+++ src/Products/Products.jsx
@@ -1,7 +1,39 @@
 import "./Products.css";
+import { AiFillStar } from "react-icons/ai";
+import { BsFillBagHeartFill } from "react-icons/bs";
 
 const Products = () => {
-  return <>Product</>;
+  return (
+    <>
+      <section className="card_container">
+        <section className="card">
+          <img
+            src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg"
+            alt="shoe"
+            className="card_img"
+          />
+          <div className="card_details">
+            <h3 className="card_title">Shoe</h3>
+            <section className="card_review">
+              <AiFillStar className="ratings_start" />
+              <AiFillStar className="ratings_start" />
+              <AiFillStar className="ratings_start" />
+              <AiFillStar className="ratings_start" />
+              <span className="total_reviews">4</span>
+            </section>
+            <section className="card_price">
+              <div className="price">
+                <del>$300</del>200
+              </div>
+              <div className="bag">
+                <BsFillBagHeartFill className="bag_icon" />
+              </div>
+            </section>
+          </div>
+        </section>
+      </section>
+    </>
+  );
 };
 
 export default Products;
