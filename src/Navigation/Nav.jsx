004082b9173064b9ec3bec272b60b33766f0d173
MominIshtiaq <momin989ishtiaq@gmail.com>
2024-02-08 02:55:35 +0500

    Navigation Complete

--- src/Navigation/Nav.jsx
+++ src/Navigation/Nav.jsx
@@ -3,25 +3,26 @@ import { FiHeart } from "react-icons/fi";
 import { AiOutlineShoppingCart, AiOutlineUserAdd } from "react-icons/ai";
 
 const Nav = () => {
-  return;
-  <>
-    <nav>
-      <div className="nav_container">
-        <input type="text" placeholder="Search" className="search_input" />
-      </div>
-      <div className="profile_container">
-        <a href="#">
-          <FiHeart className="nav_icons" />
-        </a>
-        <a href="#">
-          <AiOutlineShoppingCart className="nav_icons" />
-        </a>
-        <a href="#">
-          <AiOutlineUserAdd className="nav_icons" />
-        </a>
-      </div>
-    </nav>
-  </>;
+  return (
+    <>
+      <nav>
+        <div className="nav_container">
+          <input type="text" placeholder="Search" className="search_input" />
+        </div>
+        <div className="profile_container">
+          <a href="#">
+            <FiHeart className="nav_icons" />
+          </a>
+          <a href="#">
+            <AiOutlineShoppingCart className="nav_icons" />
+          </a>
+          <a href="#">
+            <AiOutlineUserAdd className="nav_icons" />
+          </a>
+        </div>
+      </nav>
+    </>
+  );
 };
 
 export default Nav;
